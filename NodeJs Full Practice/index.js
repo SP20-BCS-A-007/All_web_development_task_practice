1bff3e04373374405f56db30814038cb1eb5f3db
SP20-BCS-A-007 <sp20-bcs-007@cuilahore.edu.pk>
2023-01-03 19:07:29 +0500

    Nodejspractice

---
+++ NodeJs Full Practice/index.js
@@ -0,0 +1,18 @@
+// console.error(new Error("Some thing bad happen"))
+// console.log("Hello")
+
+// let array = [2,4,7,7,8,6,8,19]
+
+// let result = array.filter((item)=>{
+//    return item>=7;
+// })
+
+// console.log(result)
+
+// array.forEach((element) => {
+//     console.log(element)
+// });
+// console.log("By Map")
+// array.map((element)=>{
+// console.log(element)
+// })
